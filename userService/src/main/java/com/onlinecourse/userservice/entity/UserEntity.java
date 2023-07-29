b9d48f6115edc0c2600b1ed834bc1fd257a57f39
halarcont <herito9602@gmail.com>
2023-07-29 17:25:00 -0400

    add redis

--- userService/src/main/java/com/onlinecourse/userservice/entity/UserEntity.java
+++ userService/src/main/java/com/onlinecourse/userservice/entity/UserEntity.java
@@ -6,12 +6,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class UserEntity {
+public class UserEntity implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)

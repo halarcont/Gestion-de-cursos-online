c443a68cabeed8a90a2b3bd5a14e995d017d82ae
halarcont <herito9602@gmail.com>
2023-08-02 02:40:35 -0400

    update security keycloak

---
+++ apiGateway/src/main/java/com/onlinecourses/apigateway/SecurityConfiguration.java
@@ -0,0 +1,29 @@
+package com.onlinecourses.apigateway;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http
+                .authorizeExchange()
+                .anyExchange().authenticated()
+                .and()
+                .csrf().disable()
+                .oauth2Login()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+        return http.build();
+    }
+
+
+}
